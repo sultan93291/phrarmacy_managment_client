e4d29d1a08ddbc57943b0275b583f56d2ee9eda1
sultan93291 <abibdipto@gmail.com>
2025-01-31 11:03:48 +0600

    assesment result added to local storage

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,13 @@
 // src/app/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import loggedUserReducer from "./features/loggedInUserSlice";
+import assesMentReducer from "./features/assesmentSlice";
 import { apiSlice } from "./features/api/apiSlice"; // Import the API slice
 
 export default configureStore({
   reducer: {
     loggedInuserSlice: loggedUserReducer, // Keep your existing slice
+    assesmentSlice: assesMentReducer,
     [apiSlice.reducerPath]: apiSlice.reducer, // Add the RTK Query API slice
   },
   middleware: getDefaultMiddleware =>
